f45f7f8b1ee392df2eed0aaea07534dce02f697c
Raymond Gigliotti <ray@firestitch.com>
2026-04-16 11:52:14 -0400

    Update version to 18.0.7 and add FsAuditListModule for audit components

---
+++ src/app/modules/audits/audit-list.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { FsAuditAddComponent } from './components/audit-add/audit-add.component';
+import { FsAuditsComponent } from './components/audits/audits.component';
+import { FsAuditsSubjectDirective } from './directives/audits-subject.directive';
+
+@NgModule({
+  imports: [
+    FsAuditsComponent,
+    FsAuditAddComponent,
+    FsAuditsSubjectDirective,
+  ],
+  exports: [
+    FsAuditsComponent,
+    FsAuditAddComponent,
+    FsAuditsSubjectDirective,
+  ],
+})
+export class FsAuditListModule {}
